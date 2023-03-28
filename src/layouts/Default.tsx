e21a157d9a6b0cbc9c63696113a8a6b1f6f12242
filipe mota <fsmota@ic.ufal.br>
2023-03-28 13:31:20 -0300

    fix: home bug + professors tier

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import { Aside } from "../components/Aside/Aside"
-export function Default(){
-    return(
+export function Default() {
+    return (
         <div className="layout" style={{ justifyContent: "center" }}>
-            <div className="ms-4">
-                <Sidebar />
-            </div>
-            <div className="content">
-                <Outlet/>
-            </div>
-            <div className="Aside">
-               <Aside/>
-            </div>
+                <div>
+                    <Sidebar />
+                </div>
+                <div className="content mx-5">
+                    <Outlet />
+                </div>
+                <div>
+                    <Aside />
+                </div>
         </div>
     )
 }
