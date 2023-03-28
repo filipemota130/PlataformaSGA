900436010884fe84c612549b2bc75890189164ff
Sarah Domingos <92494941+sarahdomingos@users.noreply.github.com>
2023-03-28 12:02:13 -0300

    mudanças perfil aluno

--- src/pages/perfil/Aluno.tsx
+++ src/pages/perfil/Aluno.tsx
@@ -1,38 +1,93 @@
-import './Aluno.css'
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar_reduced from "../../components/Navbar_reduced/Navbar_reduced"
+import Card from 'react-bootstrap/esm/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export function Aluno (){
-    const Aluno1 = {
-        nome: "Karolaine",
-        matricula: 13154,
-        email: "kartulle@hotmail.com",
-        telefone: 82982347181,
-        dn: "22/09/2001",
-        sobre: "blabla",
-        disciplinas: ["dale1", "dale2", "dale3"],
-        mais: "mais info",
+export function Aluno() {
+
+    const nome = 'Aluno Waldiscleyson';
+    const bio = '[Sobre mim]';
+    const dados = {
+        matricula: 12345,
+        email: 'aw@ic.ufal.br',
+        telefone: '8295555-5555',
+        nascimento: '00/00/0000'
     }
-    return(
-        <main>
-            <div className="main">
+    const adicionais = {
+        curso: 'Ciência da computação',
+        sexo: 'Não informado',
+        etnia: 'Não informado'
+    }
+    const discPagas = ['Cálculo 1, Programação 1', 'Matemática discreta']
 
-            <div className="container-central">
-                <div className="container-foto">
+    return (
+        <>
+            <Navbar_reduced></Navbar_reduced>
+            <div className='m-5'>
+                <Container className='fundo2' style={{ borderRadius: 10, height: '100%' }}>
+                    <div>
+                        <Row xs={1} md={2} className="g-4">
+                            <Col className='text-white m-4 col-md-4' style={{ borderBlockColor: 'transparent' }}>
+                                <Card className='text-white fundo2 m-4' style={{ borderBlockColor: 'transparent' }}>
 
-                </div>
-                <div className="container-dados">
+                                    <div className="m-4">
+                                        <Card.Body className='d-flex justify-content-center fundo1 p-4' style={{ borderRadius: 10, textAlign: 'center' }}>
+                                            <div className=''>
+                                                <img src='src\assets\account_circle.png' width="200" height="200" className='' /><br></br><br></br>{nome}
+                                            </div>
 
-                </div>
-                <div className="container-sobre">
+                                        </Card.Body>
+                                    </div>
+                                    <div className="m-4">
+                                        <Card.Body className='fundo1 p-4' style={{ borderRadius: 10 }}>
+                                            <Card.Title>Dados Pessoais: </Card.Title>
+                                            <Card.Text className='ml-5'>
+                                                <ul>
+                                                    <li className='mb-2'>Matrícula SIE WEB: {dados.matricula}</li>
+                                                    <li className='mb-2'>Email: {dados.email}</li>
+                                                    <li className='mb-2'>Telefone: {dados.telefone}</li>
+                                                    <li className='mb-2'>Data de nascimento: {dados.nascimento}</li>
+                                                </ul>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </div>
+                                </Card>
+                            </Col>
 
-                </div>
-                <div className="container-disc">
+                            <Col className='text-white m-4'>
+                                <Card className='text-white fundo2 m-4'>
 
-                </div>
-                <div className="container-info">
+                                    <div className="m-4" >
+                                        <Card.Body className='d-flex flex-wrap fundo1 p-4' style={{ borderRadius: 10 }}>
+                                            {bio}
+                                        </Card.Body>
+                                    </div>
+                                    <div className="m-4">
+                                        <Card.Body className='d-flex flex-wrap fundo1 p-4' style={{ borderRadius: 10 }}>
+                                            <Card.Title>Disciplinas pagas: </Card.Title>
 
-                </div>
+                                        </Card.Body>
+                                    </div>
+
+                                </Card>
+                            </Col>
+
+                        </Row>
+
+                    </div>
+
+                    <div className='m-4'>
+                        <Card className='text-white fundo1 m-4' style={{ borderBlockColor: 'transparent' }}></Card>
+                        <Card.Body>
+                            <Card.Title>Informações adicionais: </Card.Title>
+                        </Card.Body>
+                    </div>
+                </Container>
             </div>
-            </div>
-        </main>
-    )
+        </>
+    );
 }
+
+export default Aluno;
