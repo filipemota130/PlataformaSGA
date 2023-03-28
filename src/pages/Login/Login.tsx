6e8f407690c4d6b78f1872e496dabd143076f3bf
Emmanuel1337 <emmaraujo1337@gmail.com>
2023-03-27 22:10:31 -0300

    fix & add: confirm button, undertexts

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -9,18 +9,21 @@ export function Login() {
     return (
         <>
             <Navbar_reduced />
-            <h1 className="text-center text-white" style={{marginTop:"100px", marginBottom:"50px"}}>
+            <h1 className="text-center text-white" 
+                style={{ marginTop: "100px", marginBottom: "50px" }}>
                 ENTRAR
             </h1>
             <Container className="d-flex justify-content-center">
                 <div>
-                    <InputGroup className="mb-4" style={{ width: "300px" }}>
-                        <InputGroup.Text className="fundo2 text-white border-0" id="inputGroup-sizing-sm" style={{ borderStartStartRadius: 25, borderEndStartRadius: 25 }}>
+                    <InputGroup className="mb-4" 
+                        style={{ width: "300px" }}>
+                        <InputGroup.Text className="fundo2 text-white border-0" 
+                            id="inputGroup-sizing-sm" 
+                            style={{ borderStartStartRadius: 25, borderEndStartRadius: 25 }}>
                             <img src="..\src\assets\account_circle.png" width="35" height="35" />
                         </InputGroup.Text>
-                        <Form.Control
+                        <Form.Control className="fundo2 border-0"
                             style={{ borderEndEndRadius: 25, borderStartEndRadius: 25 }}
-                            className='fundo2 border-0'
                             aria-label="Small"
                             aria-describedby="inputGroup-sizing-sm"
                             type="email"
@@ -28,21 +31,33 @@ export function Login() {
                         />
                     </InputGroup>
                     <InputGroup>
-                        <InputGroup.Text className="fundo2 text-white border-0" id="inputGroup-sizing-sm" style={{ borderStartStartRadius: 25, borderEndStartRadius: 25 }}>
-                            <img src="..\src\assets\lock.png" width="35" height="30" />
+                        <InputGroup.Text className="fundo2 text-white border-0" 
+                            id="inputGroup-sizing-sm" 
+                            style={{ borderStartStartRadius: 25, borderEndStartRadius: 25 }}>
+                            <img src="..\src\assets\lock.png" width="35" height="30"/>
                         </InputGroup.Text>
-                        <Form.Control
-                            style={{ borderEndEndRadius: 25, borderStartEndRadius: 25 }}
-                            className='fundo2 border-0'
+                        <Form.Control className="fundo2 border-0"
+                            style={{ borderEndEndRadius:25, borderStartEndRadius:25 }}
                             aria-label="Small"
                             aria-describedby="inputGroup-sizing-sm"
                             type="password"
                             placeholder="**********"
                         />
                     </InputGroup>
-                    <Button className="mb-4">
-
+                    <a href="" 
+                        style={{marginTop:"50px", textDecoration:"none"}}>
+                        Esqueci minha senha.
+                    </a>
+                    <div className="d-flex justify-content-center">
+                    <Button 
+                    style={{marginTop:"40px", height:"50px", width:"225px", borderRadius:25, position:"absolute"}}>
+                        Confirmar
                     </Button>
+                    <a href="..\src\pages\Cadastro_Aluno\Cadastro_aluno"
+                    style={{marginTop:"90px", position:"absolute", textDecoration:"none"}}>
+                        Ainda não tem conta? Crie aqui.
+                    </a>
+                    </div> 
                 </div>
             </Container>
             <div style={{ position: "absolute", bottom: 0, width: "100%" }}>
